c7f2514b7536c0814266b29da1f72093c16df5e8
KaioDD <kkaiodantas6@gmail.com>
2023-07-01 11:35:26 -0300

    Adicionar arquivos de projeto.

---
+++ SegundoProjeto/instituicao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoProjeto
+{
+    internal class Instituicao
+    {
+        public string Nome {get;set;}
+        public Endereco Endereco { get;set;}
+        public Departamento[] Departamentos { get; } = new Departamento[10];
+        private int quantidadadeDeDepartamentos = 0;
+        public void RegistrarDepartamento(Departamento d)
+        {
+            if (quantidadadeDeDepartamentos < 10)
+                Departamentos[quantidadadeDeDepartamentos++] = d; 
+        }
+        public int obterQuantidadeDeDepartamentos()
+        {
+            return quantidadadeDeDepartamentos;
+        }
+        public Departamento DepartamentoPorIndice(int indice)
+        {
+            return Departamentos[indice];
+        }
+
+
+    }
+}
